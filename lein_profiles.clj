ce30af60c065a705835864b1ba4e922b7a1f8922
Guillaume ALAUX <guillaume@alaux.net>
2016-04-16 21:24:29 +0200

    vimrc: add plugin slamhound for namespace generation

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -1,5 +1,6 @@
 {:user {:dependencies [[cljfmt "0.5.1"]
-                       [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]]
+                       [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
+                       [slamhound "1.5.5"]]
         :plugins [[cider/cider-nrepl "0.11.0"]
                   [jonase/eastwood "0.2.3"]
                   [lein-auto "0.1.2"]]}}
