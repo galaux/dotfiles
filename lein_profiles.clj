c40b580a65d238c2d364a7eecdf39a9b2376e4b5
Guillaume ALAUX <guillaume@alaux.net>
2016-04-16 16:02:34 +0200

    vimrc: setup vim-cljfmt

---
+++ lein_profiles.clj
@@ -0,0 +1,5 @@
+{:user {:dependencies [[cljfmt "0.5.1"]
+                       [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]]
+        :plugins [[cider/cider-nrepl "0.11.0"]
+                  [jonase/eastwood "0.2.3"]
+                  [lein-auto "0.1.2"]]}}
