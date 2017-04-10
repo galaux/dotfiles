a115fa11f3cad5e321404ec4eb4510159a2a491b
Guillaume ALAUX <guillaume@alaux.net>
2017-04-10 21:30:47 +0200

    Lein: add latest conf

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -1,6 +1,11 @@
-{:user {:dependencies [[cljfmt "0.5.1"]
+{:user {:dependencies [;[cljfmt "0.5.6"]
                        [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
-                       [slamhound "1.5.5"]]
-        :plugins [[cider/cider-nrepl "0.11.0"]
+                       [slamhound "1.5.5"]
+                       [pjstadig/humane-test-output "0.8.1"]
+                       [com.cemerick/pomegranate "0.3.1"]]
+        :plugins [[cider/cider-nrepl "0.14.0"]
                   [jonase/eastwood "0.2.3"]
-                  [lein-auto "0.1.2"]]}}
+                  [com.jakemccrary/lein-test-refresh "0.19.0"]]
+                  ; [lein-auto "0.1.2"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]}}
