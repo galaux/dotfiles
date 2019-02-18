5c37c994bde08c5cacb6cd45e50c349dd4a208ea
Guillaume ALAUX <guillaume@alaux.net>
2019-02-18 13:35:41 +0100

    Lein profiles update deps

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -1,11 +1,13 @@
 {:user {:dependencies [;[cljfmt "0.5.6"]
                        [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
                        [slamhound "1.5.5"]
-                       [pjstadig/humane-test-output "0.8.1"]
+                       [pjstadig/humane-test-output "0.8.3"]
                        [com.cemerick/pomegranate "0.3.1"]]
-        :plugins [[cider/cider-nrepl "0.14.0"]
+        :plugins [[cider/cider-nrepl "0.8.1"]
                   [jonase/eastwood "0.2.3"]
-                  [com.jakemccrary/lein-test-refresh "0.19.0"]]
-                  ; [lein-auto "0.1.2"]]
+                  [com.jakemccrary/lein-test-refresh "0.23.0"]
+                  [lein-auto "0.1.3"]
+                  ]
         :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]}}
+                     (pjstadig.humane-test-output/activate!)]}
+ :repl {:dependencies [[nrepl/nrepl "0.4.4"]]}}
